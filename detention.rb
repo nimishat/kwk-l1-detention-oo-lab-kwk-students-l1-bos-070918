e0b462c83962aa51dd3ffeb7d7d1ae361ec5298a
nimishat <nimishathakkar83@gmail.com>
2018-07-13 18:13:58 +0000

    automatically backed up by learn

--- detention.rb
+++ detention.rb
@@ -1,1 +1,19 @@
-#Code your detention class here
+class Detention
+  attr_accessor :test, :location, :time, :grumpy_teacher, :activity, :students, :name
+  
+  def initialize(students = [], activity = "collective punishment")
+    @activity = activity
+    @students = students
+  end
+  
+  def add_student(name)
+    add_student(name)
+  end
+  
+  def remove_student(name)
+    student_name.delete(name)
+  end
+
+    
+  
+end
